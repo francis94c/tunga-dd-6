b4215b245acc375266abb9564505e4d2642a13b0
Francis Ilechukwu <francis94c@gmail.com>
2021-06-04 22:12:08 +0100

    Models and migrations.

---
+++ database/migrations/2021_06_04_171904_create_challenge_items_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChallengeItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('challenge_items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address');
+            $table->boolean('checked');
+            $table->text('description');
+            $table->dateTime('date_of_birth');
+            $table->string('interest');
+            $table->string('email');
+            $table->string('account');
+            $table->string('credit_card_type');
+            $table->string('credit_card_number');
+            $table->string('credit_card_name');
+            $table->string('credit_card_expiration_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('challenge_items');
+    }
+}
