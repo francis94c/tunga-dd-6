771a20805d381f1ca6ab5f5b214d28623baeaca5
Francis Ilechukwu <francis94c@gmail.com>
2021-06-06 22:18:42 +0100

    Fillable array.

--- app/Models/ChallengeItem.php
+++ app/Models/ChallengeItem.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class ChallengeItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'address',
+        'checked',
+        'description',
+        'date_of_birth',
+        'interest',
+        'email',
+        'account',
+        'credit_card_type',
+        'credit_card_number',
+        'credit_card_name',
+        'credit_card_expiration_date'
+    ];
 }
