223e3d598dc274fc5f2f44e14fa426c6d3ca1301
Francis Ilechukwu <francis94c@gmail.com>
2021-06-06 22:18:32 +0100

    Queue.

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\SaveDataJob;
+use Illuminate\Http\Response;
+
+class JobController extends Controller
+{
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function runJob()
+    {
+        dispatch(new SaveDataJob('challenge.json'));
+
+        return response()->json([
+            'message' => 'Data saved from JSON file.'
+        ], Response::HTTP_OK);
+    }
+}
---
+++ app/Jobs/SaveDataJob.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\ChallengeItem;
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Facades\Cache;
+use JsonMachine\JsonMachine;
+
+class SaveDataJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($fileLocation)
+    {
+        $jsonArray = JsonMachine::fromFile($fileLocation);
+
+        $cacheKey = str_replace("/", "-", $fileLocation);
+
+        $lastSaved = Cache::get($cacheKey);
+
+        if ($lastSaved === null) {
+            $sessionId = date('YmdHis');
+        } else {
+            $sessionId = $lastSaved['session_id'];
+        }
+
+        foreach ($jsonArray as $index => $data) {
+            if ($lastSaved !== null) {
+                if ($lastSaved['index'] == $index) {
+                    info("Resuming safely...");
+                    // In case we saved but didn't record it.
+                    // Let's make sure we are not repeating ourselves.
+                    $challengeItem = ChallengeItem::where('session_id', $sessionId)->latest()->first();
+                    $lastSaved = null;
+                    if (
+                        $challengeItem && $challengeItem->name == $data['name'] &&
+                        $challengeItem->address == $data['address'] &&
+                        $challengeItem->checked == $data['checked'] &&
+                        $challengeItem->description == $data['description']
+                    ) {
+                        continue;
+                    }
+                } else {
+                    continue;
+                }
+            }
+
+            // Filter Logic
+            $dob = $data['date_of_birth'] ? Carbon::parse($data['date_of_birth']) : null;
+
+            if ($dob && $dob->age >= 18 && $dob->age <= 65) {
+                ChallengeItem::create([
+                    'name'                        => $data['name'],
+                    'address'                     => $data['address'],
+                    'checked'                     => $data['checked'],
+                    'description'                 => $data['description'],
+                    'date_of_birth'               => $dob->format('Y-m-d H:i:s') ?? null,
+                    'interest'                    => $data['interest'],
+                    'email'                       => $data['email'],
+                    'account'                     => $data['account'],
+                    'credit_card_type'            => $data['credit_card']['type'],
+                    'credit_card_number'          => $data['credit_card']['number'],
+                    'credit_card_name'            => $data['credit_card']['name'],
+                    'credit_card_expiration_date' => $data['credit_card']['expirationDate'] ? Carbon::parse($data['credit_card']['expirationDate'])->format('Y-m-d H:i:s') : null
+                ]);
+            }
+
+            Cache::put($cacheKey, [
+                'index'      => $index,
+                'session_id' => $sessionId
+            ]);
+
+            info("Processed Data: $index");
+        }
+
+        info ("Done");
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        //
+    }
+}
