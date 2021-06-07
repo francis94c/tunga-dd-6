4afa44740cc3ae87936a7f73155a02a14ad2c2f1
Francis Ilechukwu <francis94c@gmail.com>
2021-06-07 07:35:14 +0100

    Make use of start.sh for the queue service.

--- start.sh
+++ start.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 php db-test.php
-php artisan migrate
 php artisan key:generate
-php-fpm
+
+role=${CONTAINER_ROLE}
+
+if [ "$role" = "app" ]; then
+    php artisan migrate
+    php-fpm
+elif [ "$role" = "queue" ]; then
+    echo "Running the queue..."
+    php artisan queue:work --verbose --tries=3 --timeout=90
+fi
