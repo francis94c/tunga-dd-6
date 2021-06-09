39d5e62090b0664ac4e8b91d6678a413f6ef0e3a
Francis Ilechukwu <francis94c@gmail.com>
2021-06-09 10:39:55 +0100

    Quick file permission hack. not safe for production.

--- start.sh
+++ start.sh
@@ -6,6 +6,8 @@ role=${CONTAINER_ROLE}
 
 if [ "$role" = "app" ]; then
     php artisan migrate
+    chmod 777 /var/www/storage/logs/laravel.log
+    chmod -R 777 /var/www/storage/framework/sessions
     php-fpm
 elif [ "$role" = "queue" ]; then
     echo "Running the queue..."
